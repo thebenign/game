0aa4b6c7bae8519dd2b1e59ec727756707719d85
thebenign <thebenign@gmail.com>
2015-06-10 22:34:22 -0400

    Finished some more commenting

--- load_graphics.py
+++ load_graphics.py
@@ -2,7 +2,19 @@
 from pygame import image as image
 from pygame import Surface as surface
 
-def load(screen):	
+def load(screen):
+	"""
+		Loads the graphics for the game into a list,
+		then converts the graphics from png to something more
+		appropriate for blitting.
+
+		Perameters:
+			screen: a pygame Surface object for Surface.convert to use
+
+		Returns:
+			list
+
+	"""
 	tile = []
 
 	for i in range(1, 7):
